164b6e225dc4e332077c1cbf1d03df4476175ffa
Trevor Eckhoff <trevor@ceckhoff.net>
2024-05-27 03:07:13 -0400

    Change from while loop to timer in thread

---
+++ src/PyPlotAnaVis/Threads.py
@@ -0,0 +1,165 @@
+﻿import math
+
+import numpy
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+DEFAULT_AMPLITUDE = 1.0
+DEFAULT_FREQUENCY = 1.0
+DEFAULT_OFFSET = 0.0
+DEFAULT_SIMULATION_RATE = int(1000 / 80)  # Default simulation rate to ~80Hz
+DEFAULT_SIMULATION_RATE_HZ = 80.0  # Default simulation rate to ~80Hz
+
+
+class StartSimulationSignal(QtCore.QObject):
+    Begin = QtCore.pyqtSignal()
+
+
+class SimulationEndedSignal(QtCore.QObject):
+    Ended = QtCore.pyqtSignal()
+
+
+class DataStreamThread(QtCore.QThread):
+    def __init__(self, output_timestamp_list: list[float], output_value_list: list[float]):
+        super().__init__()
+
+        self.ShouldSimulate = False
+        self.StartSimulationSignal = StartSimulationSignal()
+
+        self.DataStreamWorker = SimulateDataStreamWorker(output_timestamp_list, output_value_list, self)
+        self.DataStreamWorker.moveToThread(self)
+
+    def update_inputs(self, new_amplitude: float, new_frequency: float, new_offset: float,
+                      new_simulation_rate: float) -> None:
+        self.DataStreamWorker.set_amplitude(new_amplitude)
+        self.DataStreamWorker.set_frequency(new_frequency)
+        self.DataStreamWorker.set_offset(new_offset)
+
+        if new_simulation_rate < 1:
+            new_simulation_rate = 1
+
+        self.DataStreamWorker.set_simulation_rate(new_simulation_rate)
+
+    def get_simulation_ended_signal_from_worker(self) -> SimulationEndedSignal:
+        return self.DataStreamWorker.get_simulation_ended_signal()
+
+    def toggle_simulation(self, should_run: bool) -> None:
+        if should_run:
+            self.StartSimulationSignal.Begin.emit()
+            return
+
+        self.ShouldSimulate = False
+        self.DataStreamWorker.end_timer()
+
+    def run(self):
+        def start_worker() -> None:
+            self.DataStreamWorker.start_simulation()
+
+        self.StartSimulationSignal.Begin.connect(start_worker)
+
+        super().run()
+
+
+class SimulateDataStreamWorker(QtCore.QObject):
+    """
+    A worker that simulates a continuous data stream. Meant to be run on a separate thread.
+    """
+
+    def __init__(self, output_timestamp_list: list[float], output_value_list: list[float],
+                 owning_thread: DataStreamThread):
+        super().__init__()
+
+        self.Amplitude: float = DEFAULT_AMPLITUDE
+        self.Frequency: float = DEFAULT_FREQUENCY
+        self.Offset: float = DEFAULT_OFFSET
+        self.DataRate: int = DEFAULT_SIMULATION_RATE
+        self.DataTimer = QtCore.QTimer()
+        self.DataTimer.moveToThread(owning_thread)
+        self.DataTimer.setInterval(self.DataRate)
+        self.DataTimer.timeout.connect(self.simulate)
+        self.PreviousTimestamp = -1.0
+        self.simulation_begin_timestamp = -1.0
+
+        self.OwningThread = owning_thread
+        self.SimulationEndedSignal = SimulationEndedSignal()
+
+        self.OutputTimestampList: list[float] = output_timestamp_list
+        self.OutputValueList: list[float] = output_value_list
+
+    def get_sine_value(self, time: float) -> float:
+        """
+        Returns the result of a sin function account for amplitude, frequency, and offset.
+        :param time: Time, in seconds.
+        :return: The resulting value of the sin.
+        """
+        return self.Amplitude * numpy.sin((2 * numpy.pi * self.Frequency) * time + self.Offset)
+
+    def set_amplitude(self, new_amplitude: float) -> None:
+        self.Amplitude = new_amplitude
+
+    def set_frequency(self, new_frequency: float) -> None:
+        self.Frequency = new_frequency
+
+    def set_offset(self, new_offset: float) -> None:
+        self.Offset = new_offset
+
+    def update_lists(self, new_timestamp_list: list[float], new_value_list: list[float]) -> None:
+        self.OutputTimestampList = new_timestamp_list
+        self.OutputValueList = new_value_list
+
+    def set_simulation_rate(self, new_simulation_rate: float) -> None:
+        """
+        Sets the time in milliseconds between simulation cycles.
+        :param new_simulation_rate: The simulation rate in hertz.
+        """
+        self.DataRate = int((1 / new_simulation_rate) * 1000)
+        self.DataTimer.setInterval(self.DataRate)
+
+    def get_simulation_ended_signal(self) -> SimulationEndedSignal:
+        return self.SimulationEndedSignal
+
+    def start_simulation(self) -> None:
+        """
+        Starts the continuous data stream simulation. Runs until end_simulation() is called.
+        """
+        if self.OwningThread.ShouldSimulate:
+            return
+
+        self.OwningThread.ShouldSimulate = True
+        self.simulation_begin_timestamp = QtCore.QDateTime.currentMSecsSinceEpoch()
+        previous_timestamp = -1.0
+
+        """while self.OwningThread.ShouldSimulate:
+            time_elapsed = (QtCore.QDateTime.currentMSecsSinceEpoch() - simulation_begin_timestamp) / 1000
+
+            if math.isclose(time_elapsed, previous_timestamp):
+                continue
+
+            sine_value = self.get_sine_value(time_elapsed)
+
+            previous_timestamp = time_elapsed
+
+            self.OutputTimestampList.append(time_elapsed)
+            self.OutputValueList.append(sine_value)
+
+            self.OwningThread.wait(self.DataRate)
+
+        self.SimulationEndedSignal.Ended.emit()"""
+        self.DataTimer.start()
+
+    def simulate(self) -> None:
+        if not self.OwningThread.ShouldSimulate:
+            self.end_timer()
+
+        time_elapsed = (QtCore.QDateTime.currentMSecsSinceEpoch() - self.simulation_begin_timestamp) / 1000
+
+        sine_value = self.get_sine_value(time_elapsed)
+
+        self.PreviousTimestamp = time_elapsed
+
+        self.OutputTimestampList.append(time_elapsed)
+        self.OutputValueList.append(sine_value)
+
+    def end_timer(self) -> None:
+        self.DataTimer.stop()
+        self.SimulationEndedSignal.Ended.emit()
+        return
--- src/PyPlotAnaVis/Widgets.py
+++ src/PyPlotAnaVis/Widgets.py
@@ -1,15 +1,26 @@
-﻿import pyqtgraph
+﻿import math
+
+import numpy
+import pyqtgraph
 import images_qr
-from PyQt5 import QtGui, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets
+import Threads
+
+CACHE_COUNTER_DEFAULT_STATE = 9  # Our first cache is going to be created at 9 seconds, because we start at 0.
+CACHE_INCREMENT = 10  # We want to cache our data off of the graph every 10 seconds.
+GRAPH_RENDER_RATE = int(1000 / 60)  # The graph will be rendered in ~60FPS.
+KEEP_FROM_CACHE: int = 50  # How many cached data points to rollover into the current data to ensure there are no gaps.
+WINDOW_SIZE_X: int = 700  # The initial window size on the X axis.
+WINDOW_SIZE_Y: int = 300  # The initial window size on the Y axis.
 
 
 class AppWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
+        # Create the window.
         self.setWindowTitle("Python Plot AnaVis")
-        self.setMinimumSize(700, 300)
-        self.setGeometryFromScreenResolution(700, 300)
+        self.setGeometryFromScreenResolution()
 
         self.WidgetStack = QtWidgets.QStackedWidget(self)
 
@@ -18,54 +29,215 @@ class AppWindow(QtWidgets.QMainWindow):
         self.PlotterPage = QtWidgets.QWidget(self)
 
         self.Page1HorizontalLayout = QtWidgets.QHBoxLayout(self.PlotterPage)
-        self.Page1LeftLayout = PlottingInput(self.PlotterPage)
+        self.PlottingControls = PlottingInput(self.PlotterPage)
 
-        self.PlotGraph = pyqtgraph.PlotWidget()
+        self.PlottingControls.TogglePlottingButton.clicked.connect(self.toggle_plotting)
 
-        self.Page1HorizontalLayout.addLayout(self.Page1LeftLayout)
+        self.PlotGraph = PlotGraph()
+
+        self.DataPoints = []
+        self.TimePoints = []
+
+        self.PlottedLine = self.PlotGraph.plot(self.DataPoints, self.TimePoints)
+        self.CachedLine = self.PlotGraph.plot()
+
+        self.Page1HorizontalLayout.addLayout(self.PlottingControls)
         self.Page1HorizontalLayout.addWidget(self.PlotGraph)
+
+        self.DataStreamThread = Threads.DataStreamThread(self.TimePoints, self.DataPoints)
+        self.DataStreamThread.get_simulation_ended_signal_from_worker().Ended.connect(self.enable_start_button)
+        self.DataStreamThread.start()
+
+        self.GraphUpdateTimer = QtCore.QTimer()
+        self.GraphUpdateTimer.setInterval(GRAPH_RENDER_RATE)
+        self.GraphUpdateTimer.timeout.connect(self.update_graph)
+
+        self.SimulationStarted: int = 0
+
+        self.CacheCounter = CACHE_COUNTER_DEFAULT_STATE
+        self.PreviousCacheIndex = 0
+        self.CachedLines = []
 
         self.WidgetStack.addWidget(self.PlotterPage)
 
     # Pep8 suggests function names are lowercase, this has been ignored here for parity with PyQt5 functions.
-    def setGeometryFromScreenResolution(self, default_window_size_x: int, default_window_size_y: int) -> None:
+    def setGeometryFromScreenResolution(self) -> None:
         """Uses the screen resolution from PyQt5 to place the window in the middle of the screen.
         """
         screen = QtWidgets.QApplication.primaryScreen()
         screen_size_x, screen_size_y = screen.size().width(), screen.size().height()
-        position_x = int((screen_size_x / 2) - (default_window_size_x / 2))
-        position_y = int((screen_size_y / 2) - (default_window_size_y / 2))
-        self.setGeometry(position_x, position_y, default_window_size_x, default_window_size_y)
+        position_x = int((screen_size_x / 2) - (WINDOW_SIZE_X / 2))
+        position_y = int((screen_size_y / 2) - (WINDOW_SIZE_Y / 2))
+        self.setGeometry(position_x, position_y, WINDOW_SIZE_X, WINDOW_SIZE_Y)
+
+    def toggle_plotting(self) -> None:
+        self.PlottingControls.toggle_plotting()
+        self.DataStreamThread.toggle_simulation(self.PlottingControls.IsPlotting)
+        self.DataStreamThread.update_inputs(self.PlottingControls.get_amplitude(),
+                                            self.PlottingControls.get_frequency(),
+                                            self.PlottingControls.get_offset(),
+                                            self.PlottingControls.get_datastream_simulation_rate())
+
+        self.PlotGraph.setYRange(-self.PlottingControls.get_amplitude() - 0.5,
+                                 self.PlottingControls.get_amplitude() + 0.5)
+
+        if self.PlottingControls.IsPlotting:
+            self.PlottedLine.clear()
+            self.SimulationStarted = QtCore.QDateTime.currentSecsSinceEpoch()
+            self.GraphUpdateTimer.start()
+            return
+
+        # self.PlottingControls.toggle_plotting_button_enabled(False)
+        self.GraphUpdateTimer.stop()
+
+    def reset_plotting(self) -> None:
+        # Clear our lists to facilitate new data
+        self.TimePoints.clear()
+        self.DataPoints.clear()
+        self.CachedLines.clear()
+        # Reset our lines
+        self.CachedLine.clear()
+        self.PlotGraph.removeItem(self.CachedLine)
+        del self.CachedLine
+        self.CachedLine = self.PlotGraph.plot()
+        self.PlottedLine.clear()
+        self.PlotGraph.removeItem(self.PlottedLine)
+        del self.PlottedLine
+        self.PlottedLine = self.PlotGraph.plot()
+
+        # Update the thread and counter to default state
+        self.DataStreamThread.DataStreamWorker.update_lists(self.TimePoints, self.DataPoints)
+        self.CacheCounter = CACHE_COUNTER_DEFAULT_STATE
+
+    def enable_start_button(self) -> None:
+        print("test")
+        self.PlottingControls.toggle_plotting_button_enabled(True)
+        self.show_cached_lines()
+
+    def add_cached_lines_to_lists(self, time_points: list[float], data_points: list[float], initial_cache: bool):
+        for cache in self.CachedLines:
+            if initial_cache:
+                time_points.extend(cache[0])
+                data_points.extend(cache[1])
+                initial_cache = False
+                continue
+            time_points.extend(cache[0][KEEP_FROM_CACHE:])
+            data_points.extend(cache[1][KEEP_FROM_CACHE:])
+
+    def show_cached_lines(self):
+        total_time_points = []
+        total_data_points = []
+        initial_cache = True
+        self.add_cached_lines_to_lists(total_time_points, total_data_points, initial_cache)
+
+        if len(total_time_points) == 0:
+            total_time_points.extend(self.TimePoints)
+            total_data_points.extend(self.DataPoints)
+        else:
+            total_time_points.extend(self.TimePoints[KEEP_FROM_CACHE:])
+            total_data_points.extend(self.DataPoints[KEEP_FROM_CACHE:])
+
+        self.reset_plotting()
+        self.PlottedLine.setData(total_time_points, total_data_points)
+
+    def save_data_to_file(self):
+        print("saving")
+        saving_time_points = []
+        saving_data_points = []
+        initial_cache = True if self.CacheCounter <= 60 else False
+        self.add_cached_lines_to_lists(saving_time_points, saving_data_points, initial_cache)
+        numpy.save(str(self.SimulationStarted), numpy.asarray([saving_time_points, saving_data_points]))
+
+    def update_graph(self):
+        current_max_point = len(self.TimePoints) - 1
+
+        if current_max_point < 1:
+            return
+
+        current_second = math.floor(self.TimePoints[current_max_point])
+
+        if current_second > self.CacheCounter:
+            self.CacheCounter += 10
+            print()
+            self.CachedLines.append(self.PlottedLine.getData())
+            self.CachedLine.clear()
+            self.CachedLine.setData(self.PlottedLine.getData()[0], self.PlottedLine.getData()[1])
+            self.PlottedLine.clear()
+            self.TimePoints = self.TimePoints[current_max_point - KEEP_FROM_CACHE:]
+            self.DataPoints = self.DataPoints[current_max_point - KEEP_FROM_CACHE:]
+            self.DataStreamThread.DataStreamWorker.update_lists(self.TimePoints, self.DataPoints)
+            if (self.CacheCounter - CACHE_COUNTER_DEFAULT_STATE) % 60 == 0:
+                self.save_data_to_file()
+
+        self.PlottedLine.setData(self.TimePoints[:-1], self.DataPoints[:-1])
+        self.PlotGraph.setXRange(self.TimePoints[len(self.TimePoints) - 1] - 8,
+                                 self.TimePoints[len(self.TimePoints) - 1] + 8)
+
+
+class PlotGraph(pyqtgraph.PlotWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setBackground("w")
+        self.setTitle("Plot Graph")
+        self.setLabel("bottom", "Time (sec.)")
+        self.setLabel("left", "Data")
 
 
 class PlottingInput(QtWidgets.QVBoxLayout):
     def __init__(self, parent: QtWidgets.QWidget):
         super().__init__(parent)
 
-        self.IsPlotting = False
+        self.IsPlotting: bool = False
 
-        self.AmplitudeInputField = FloatInputField("Amplitude", 1.0, parent)
+        self.AmplitudeInputField: FloatInputField = FloatInputField("Amplitude",
+                                                                    Threads.DEFAULT_AMPLITUDE, parent)
         self.addLayout(self.AmplitudeInputField)
 
-        self.FrequencyInputField = FloatInputField("Frequency", 1.0, parent)
+        self.FrequencyInputField: FloatInputField = FloatInputField("Frequency",
+                                                                    Threads.DEFAULT_FREQUENCY, parent)
         self.addLayout(self.FrequencyInputField)
 
-        self.OffsetInputField = FloatInputField("Offset", 0.0, parent)
+        self.OffsetInputField: FloatInputField = FloatInputField("Offset",
+                                                                 Threads.DEFAULT_OFFSET, parent)
         self.addLayout(self.OffsetInputField)
 
-        self.ButtonStartIcon = QtGui.QIcon(':/img/Play.png')
-        self.ButtonStopIcon = QtGui.QIcon(':/img/Stop.png')
+        self.DataStreamInputField: FloatInputField = FloatInputField("Data Rate (Hz)",
+                                                                     Threads.DEFAULT_SIMULATION_RATE_HZ, parent)
+        self.addLayout(self.DataStreamInputField)
 
-        self.TogglePlottingButton = QtWidgets.QPushButton(parent)
+        self.ButtonStartIcon: QtGui.QIcon = QtGui.QIcon(':/img/Play.png')
+        self.ButtonStopIcon: QtGui.QIcon = QtGui.QIcon(':/img/Stop.png')
+
+        self.TogglePlottingButton: QtWidgets.QPushButton = QtWidgets.QPushButton(parent)
         self.TogglePlottingButton.setText("Start Plotting")
         self.TogglePlottingButton.setIcon(self.ButtonStartIcon)
-        self.TogglePlottingButton.clicked.connect(self.toggle_plotting)
         self.addWidget(self.TogglePlottingButton)
 
     def toggle_plotting(self):
         self.TogglePlottingButton.setText("Stop Plotting" if not self.IsPlotting else "Start Plotting")
         self.TogglePlottingButton.setIcon(self.ButtonStopIcon if not self.IsPlotting else self.ButtonStartIcon)
         self.IsPlotting = not self.IsPlotting
+        self.AmplitudeInputField.toggle_enabled(not self.IsPlotting)
+        self.FrequencyInputField.toggle_enabled(not self.IsPlotting)
+        self.OffsetInputField.toggle_enabled(not self.IsPlotting)
+        self.DataStreamInputField.toggle_enabled(not self.IsPlotting)
+
+    def get_amplitude(self):
+        return float(self.AmplitudeInputField.InputField.text())
+
+    def get_frequency(self):
+        return float(self.FrequencyInputField.InputField.text())
+
+    def get_offset(self):
+        return float(self.OffsetInputField.InputField.text())
+
+    def get_datastream_simulation_rate(self):
+        return float(self.DataStreamInputField.InputField.text())
+
+    def toggle_plotting_button_enabled(self, is_enabled: bool):
+        print("Toggling plotting button to " + str(is_enabled))
+        self.TogglePlottingButton.setEnabled(is_enabled)
 
 
 class FloatInputField(QtWidgets.QHBoxLayout):
@@ -93,3 +265,6 @@ class FloatInputField(QtWidgets.QHBoxLayout):
 
         spacer_3 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.addItem(spacer_3)
+
+    def toggle_enabled(self, is_enabled: bool) -> None:
+        self.InputField.setEnabled(is_enabled)
